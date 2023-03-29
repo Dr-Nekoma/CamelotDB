f3427a2e45c925425a530b12cb0f8c04e2024a89
Marcos Magueta <maguetamarcos@gmail.com>
2023-03-28 22:02:05 -0300

    bootstrap dune project

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A Nix-flake-based OCaml development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ ocaml ocamlformat ] ++
+          (with pkgs.ocamlPackages; [ dune_3 odoc ocaml-lsp ]);
+
+        shellHook = ''
+          ${pkgs.ocaml}/bin/ocaml --version
+        '';
+      };
+    });
+}
